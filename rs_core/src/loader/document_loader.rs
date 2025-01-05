01803c114d57ba399ecf0fa2099ef1bcf71718a3
Mohamed Rhoulam <mohamedrhoulam.55@gmail.com>
2025-01-06 00:39:25 +0100

    update DocumentLoader to support loading from files and add next method for document retrieval

--- rs_core/src/loader/document_loader.rs
+++ rs_core/src/loader/document_loader.rs
@@ -18,11 +18,12 @@ impl DocumentLoader {
     pub fn new(source: String) -> Self {
         Self {
             source,
-            documents: VecDeque::new(),
+            documents: VecDeque::<Document>::new(),
         }
     }
 
-    pub fn load_document(&mut self) {
+    // this is terrible
+    pub fn load(&mut self) {
         if Path::new(&self.source).is_dir() {
             for entry in fs::read_dir(&self.source).expect("Unable to read directory") {
                 let entry = entry.expect("Unable to read entry");
@@ -42,7 +43,23 @@ impl DocumentLoader {
                     });
                 }
             }
-        } else {
+        } else if Path::new(&self.source).is_file() {
+            let document_id = Uuid::max().simple().to_string();
+            let document_content = fs::read_to_string(&self.source).expect("Unable to read file");
+            let document_name = Path::new(&self.source)
+                .file_name()
+                .and_then(|name| name.to_str())
+                .map(|name| name.to_string())
+                .expect("Unable to get file name");
+            self.documents.push_back(Document {
+                document_id,
+                document_name,
+                document_content,
+            });
         }
+    }
+
+    pub fn next(&mut self) -> Option<Document> {
+        self.documents.pop_front()
     }
 }
